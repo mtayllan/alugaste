e46d04d63c6f42b26e156dcf6ca5e43660fb27fd
mtayllan <messiastayllan@gmail.com>
2022-05-28 14:26:11 -0300

    Move host login to webActions

--- web/index.js
+++ web/index.js
@@ -2,11 +2,11 @@ import 'dotenv/config'
 import express from 'express';
 import expressLayouts from 'express-ejs-layouts';
 import { getRooms } from 'alugaste-core/rooms.js';
-import { login as hostLogin } from 'alugaste-core/host/authentication.js'
 import { login as guestLogin } from 'alugaste-core/guest/authentication.js'
 import cookieParser from 'cookie-parser';
 import { hostAuthentication } from './middlewares/hostAuthentication.js';
 import { guestAuthentication } from './middlewares/guestAuthentication.js';
+import * as hostLogin from './actions/host/login.js';
 
 const app = express();
 const port = 3000;
@@ -26,13 +26,8 @@ app.get('/', (req, res) => {
   res.render('index', { rooms })
 })
 
-app.get('/host/login', (req, res) => {
-  if (req.hostSignedIn) {
-    res.redirect('/');
-  } else {
-    res.render('host/login');
-  }
-})
+app.get('/host/login', hostLogin.getLogin);
+app.post('/host/login', hostLogin.postLogin);
 
 app.get('/guest/login', (req, res) => {
   if (req.guestSignedIn) {
@@ -41,17 +36,6 @@ app.get('/guest/login', (req, res) => {
     res.render('guest/login');
   }
 })
-
-app.post('/host/login', async (req, res) => {
-  const formData = { email: req.body.email, password: req.body.password };
-  const response = await hostLogin(formData);
-  if (response === 'not_found') {
-    res.render('host/login', { error: true, formData })
-  } else {
-    res.cookie('_alugaste_host_session', response);
-    res.redirect('/');
-  }
-});
 
 app.post('/guest/login', async (req, res) => {
   const formData = { email: req.body.email, password: req.body.password }
