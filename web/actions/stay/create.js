0f7b07786efd22fead0f6ff2dd2cb5f688992879
Arthurpbezerra <arthurpintobezerra@gmail.com>
2022-06-09 19:25:52 -0300

    calculate numNights by date

--- web/actions/stay/create.js
+++ web/actions/stay/create.js
@@ -1,7 +1,10 @@
 import { findRoom } from "alugaste-core/rooms/rooms.js";
-
+import { parse,differenceInDays } from "date-fns";
 export const getCreate = async (req, res) => {
     const room = await findRoom(req.params.id);
-    res.render('stay/create', { room, start_date: req.query.start_date, end_date: req.query.end_date, total_value: 500 , total_guests: req.query.total_guests})
-    
+    const start_date = parse(req.query.start_date, "dd/MM/y",new Date())
+    const end_date = parse(req.query.end_date, "dd/MM/y",new Date())
+    const numNights = differenceInDays(end_date,start_date)
+    const total_value= (numNights * room.pricePerNight)
+    res.render('stay/create', { room, start_date: req.query.start_date, end_date: req.query.end_date, total_value, total_guests: req.query.total_guests, numNights})
   }
