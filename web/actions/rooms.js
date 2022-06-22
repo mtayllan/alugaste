57c5126b8e7d724631cd8cd23b2b5cb3b64bf04e
Tayllan <messiastayllan@gmail.com>
2022-06-22 19:58:47 +0000

    Use environment variable on rooms actions

--- web/actions/rooms.js
+++ web/actions/rooms.js
@@ -1,17 +1,17 @@
 import fetch from 'node-fetch'
 
 export const getRooms = async (req, res) => {
-  const rooms = await (await fetch('http://localhost:4000/rooms?search' + req.query.search)).json();
+  const rooms = await (await fetch(`${process.env.PRIVATE_SERVER_URL}/rooms?search${req.query.search}`)).json();
   res.render('index', { rooms })
 }
 
 export const getRoomsJson = async (req, res) => {
-  const url = `http://localhost:4000/rooms?search${req.query.search ?? ''}&page=${req.query.page ?? ''}`; 
+  const url = `${process.env.PRIVATE_SERVER_URL}/rooms?search${req.query.search ?? ''}&page=${req.query.page ?? ''}`; 
   const rooms = await (await fetch(url)).json();
   res.json(rooms);
 }
 
 export const getRoom = async (req, res) => {
-  const room = await (await fetch('http://localhost:4000/rooms/' + req.params.id)).json();
+  const room = await (await fetch(`${process.env.PRIVATE_SERVER_URL}/rooms/${req.params.id}`)).json();
   res.render('rooms/view', { room })
 }
