c0f8d854d9226d0ad4599a6b7e81e9e714abdb4b
mtayllan <messiastayllan@gmail.com>
2022-06-25 08:59:01 -0300

    Add createRoom to core_web

--- core_web/rooms.js
+++ core_web/rooms.js
@@ -1,6 +1,9 @@
 import express from 'express'
-import { findRoom, listRooms } from 'core/rooms.js';
+import { findRoom, listRooms, createRoom } from 'core/rooms.js';
+import multer from 'multer';
+
 const router = express.Router();
+const upload = multer({ dest: 'uploads/', limits: { fileSize: 1024 * 1024 } })
 
 router.get('/', async (req, res) => {
   const hostId = req.params.host_id;
@@ -12,6 +15,27 @@ router.get('/:id', async (req, res) => {
   const id = req.params.id;
   const room = await findRoom(id);
   res.json(room);
+});
+
+router.post('/', upload.array('photos', 4), async (req, res) => {
+  const formData = {
+    name: req.body.name,
+    description: req.body.description,
+    maxGuests: parseInt(req.body.maxGuests),
+    totalBathrooms: parseInt(req.body.totalBathrooms),
+    totalRooms: parseInt(req.body.totalRooms),
+    totalBeds: parseInt(req.body.totalBeds),
+    others: req.body.others,
+    pricePerNight: parseInt(req.body.pricePerNight),
+    minNights: parseInt(req.body.minNights),
+    maxNights: parseInt(req.body.maxNights),
+    hostId: req.body.hostId,
+    photos: req.files
+  };
+
+  const room = await createRoom(formData);
+
+  res.json({}).status(201);
 });
 
 export default router;
--- web/actions/host/rooms.js
+++ web/actions/host/rooms.js
@@ -1,4 +1,5 @@
-import { createRoom } from 'alugaste-core/rooms.js'
+import { FormData, fileFromSync } from 'node-fetch';
+import fetchApi from '../../fetchApi.js';
 
 export const getNewRoom = (req, res) => {
   if (req.hostSignedIn) {
@@ -10,22 +11,28 @@ export const getNewRoom = (req, res) => {
 
 export const postRoom = async (req, res) => {
   if (req.hostSignedIn) {
-    const formData = {
-      name: req.body.name,
-      description: req.body.description,
-      maxGuests: parseInt(req.body.maxGuests),
-      totalBathrooms: parseInt(req.body.totalBathrooms),
-      totalRooms: parseInt(req.body.totalRooms),
-      totalBeds: parseInt(req.body.totalBeds),
-      others: req.body.others,
-      pricePerNight: parseInt(req.body.pricePerNight),
-      minNights: parseInt(req.body.minNights),
-      maxNights: parseInt(req.body.maxNights),
-      hostId: req.currentHost._id,
-      photos: req.files,
-    };
+    const formData = new FormData();
+    req.files.forEach((file) => {
+      const fileObj = fileFromSync(file.path, file.mimetype);
+      formData.append('photos', fileObj)
+    });
+    formData.set('name', req.body.name);
+    formData.set('description', req.body.description);
+    formData.set('maxGuests',  parseInt(req.body.maxGuests));
+    formData.set('totalBathrooms', parseInt(req.body.totalBathrooms));
+    formData.set('totalRooms',  parseInt(req.body.totalRooms));
+    formData.set('totalBeds',  parseInt(req.body.totalBeds));
+    formData.set('others',  req.body.others ?? '');
+    formData.set('pricePerNight', parseInt(req.body.pricePerNight));
+    formData.set('minNights', parseInt(req.body.minNights));
+    formData.set('maxNights', parseInt(req.body.maxNights));
+    formData.set('hostId', req.currentHost._id);
 
-    await createRoom(formData);
+    await fetchApi('/rooms', {
+      method: 'POST',
+      body: formData
+    })
+
     res.redirect('/');
   } else {
     res.redirect('/host/login');
